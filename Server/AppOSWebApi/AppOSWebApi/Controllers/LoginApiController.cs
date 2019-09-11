ece24912f82b98590e3a1fd84f62537753313d8a
Gabriel <p.set57@gmail.com>
2019-09-10 23:36:53 -0300

    Estrutura Json

---
+++ Server/AppOSWebApi/AppOSWebApi/Controllers/LoginApiController.cs
@@ -0,0 +1,32 @@
+﻿using AppOSWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace AppOSWebApi.Controllers
+{
+    public class LoginApiController :ApiController
+    {
+
+        [HttpGet]
+        [Route("api/LoginApiController/Entrar")]
+        public PostApiController<EmpresaModels> Entrar(EmpresaLoginModels values)
+        {
+            return new PostApiController<EmpresaModels> {
+                Mensagem = "Logado Com Sucesso",
+                Result = true,
+                Objeto = new EmpresaModels() {
+                    CPFCNPJ = "teste",
+                    Data_Ultima_Atualizacao = DateTime.Now,
+                    Endereco = new EnderecoModels(),
+                    Telefone = "000000"
+                                     
+                }
+                           
+            }; 
+        }
+
+    }
+}
---
+++ Server/AppOSWebApi/AppOSWebApi/Models/EmpresaModels.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppOSWebApi.Models
+{
+    public class EmpresaModels
+    {
+        public String CPFCNPJ { get; set; }
+        public EnderecoModels Endereco { get; set; } 
+        public String Telefone { get; set; }
+        public DateTime Data_Ultima_Atualizacao { get; set; }
+        
+    }
+
+    public class EmpresaLoginModels
+    {
+        public String CPFCNPJ { get; set; }
+        public String Senha { get; set; }
+    }
+}
