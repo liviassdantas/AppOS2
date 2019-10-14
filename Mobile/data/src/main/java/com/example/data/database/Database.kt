b88c6772353ccbf4fe8e0e404dda7b81f3d8036e
Gabriel <p.set57@gmail.com>
2019-10-13 22:29:41 -0300

    Criação do banco de dados e entidade Empresa

---
+++ Mobile/data/src/main/java/com/example/data/database/Database.kt
@@ -0,0 +1,11 @@
+package com.example.data.database
+
+import android.content.Context
+import androidx.room.Room
+
+class Database {
+//    lateinit var context: Context
+//    val db = Room.databaseBuilder(
+//        //context, Database::class.java, "os_bd"
+//    ).build()
+}
---
+++ Mobile/data/src/main/java/com/example/data/entity/Empresa.kt
@@ -0,0 +1,20 @@
+package com.example.data.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Empresa")
+data class Empresa(
+    @PrimaryKey var cpf_cnpj: String? = null,
+    @ColumnInfo(name = "nome")var nome: String? = null,
+    @ColumnInfo(name = "telefone")var telefone: String? = null,
+    @ColumnInfo(name = "cep")var cep: String? = null,
+    @ColumnInfo(name = "email")var email: String? = null,
+    @ColumnInfo(name = "endereco")var endereco: String? = null,
+    @ColumnInfo(name = "estado")var estado: String? = null,
+    @ColumnInfo(name = "cidade")var cidade: String? = null,
+    @ColumnInfo(name = "bairro")var bairro: String? = null,
+    @ColumnInfo(name = "num_residencia")var num_residencia: String? = null,
+    @ColumnInfo(name = "senha")var senha: String? = null
+)
