b135b5302fe3fd3b6147649a2cd190dc78f54dfb
Gabriel Xavier <xavier@arch.localdomain>
2019-11-20 18:42:10 -0300

    layouts em desenvolvimento

---
+++ StatusOS/app/src/main/java/com/tcc/statusos/Entrada.kt
@@ -0,0 +1,12 @@
+package com.tcc.statusos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Entrada : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_entrada)
+    }
+}
